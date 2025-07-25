b940f1697ff9796abd6b2f54961a2ae014464e11
NathanFallet <contact@nathanfallet.me>
2025-07-25 11:06:59 +0200

    feat: first tutorial

---
+++ amqp-client/src/commonTest/kotlin/dev/kourier/amqp/tutorials/HelloWorldTest.kt
@@ -0,0 +1,64 @@
+package dev.kourier.amqp.tutorials
+
+import dev.kourier.amqp.Properties
+import dev.kourier.amqp.connection.amqpConfig
+import dev.kourier.amqp.connection.createAMQPConnection
+import io.ktor.utils.io.core.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.runBlocking
+import kotlin.test.Test
+
+class HelloWorldTest {
+
+    val queueName = "hello"
+
+    suspend fun send(coroutineScope: CoroutineScope) {
+        val config = amqpConfig {
+            server {
+                host = "localhost"
+            }
+        }
+        val connection = createAMQPConnection(coroutineScope, config)
+        val channel = connection.openChannel()
+
+        channel.queueDeclare(queueName, durable = false, exclusive = false, autoDelete = false, arguments = emptyMap())
+        val message = "Hello World!"
+        channel.basicPublish(message.toByteArray(), exchange = "", routingKey = queueName, properties = Properties())
+        println("[x] Sent '$message'")
+
+        channel.close()
+        connection.close()
+    }
+
+    suspend fun receive(coroutineScope: CoroutineScope) {
+        val config = amqpConfig {
+            server {
+                host = "localhost"
+            }
+        }
+        val connection = createAMQPConnection(coroutineScope, config)
+        val channel = connection.openChannel()
+
+        channel.queueDeclare(queueName, durable = false, exclusive = false, autoDelete = false, arguments = emptyMap())
+        println("[*] Waiting for messages. To exit press CTRL+C")
+
+        val consumer = channel.basicConsume(queueName, noAck = true)
+        for (delivery in consumer) {
+            val message: String = delivery.message.body.decodeToString()
+            println("[x] Received '$message'")
+
+            // For this test, we will break after the first message (otherwise it would run indefinitely)
+            break
+        }
+
+        channel.close()
+        connection.close()
+    }
+
+    @Test
+    fun testHelloWorld() = runBlocking {
+        send(this)
+        receive(this)
+    }
+
+}
---
+++ docs/tutorials/index.md
@@ -0,0 +1,11 @@
+---
+title: Tutorials
+nav_order: 10
+---
+
+# Tutorials
+
+These tutorials cover the basics of creating messaging applications using AMQP/RabbitMQ with kourier.
+
+To follow these tutorials, we recommend having a RabbitMQ server running locally or in a Docker container. You can find
+instructions on how to set up RabbitMQ in the [RabbitMQ documentation](https://www.rabbitmq.com/docs/download).
